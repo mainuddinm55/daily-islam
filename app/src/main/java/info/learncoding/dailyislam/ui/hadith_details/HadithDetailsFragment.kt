55834d55246a1e072f6ca5016dce3b52c7061663
Md Mainuddin <mainuddinm55@gmail.com>
2021-06-03 22:25:40 +0600

    add grades by

--- app/src/main/java/info/learncoding/dailyislam/ui/hadith_details/HadithDetailsFragment.kt
+++ app/src/main/java/info/learncoding/dailyislam/ui/hadith_details/HadithDetailsFragment.kt
@@ -28,18 +28,36 @@ class HadithDetailsFragment : Fragment() {
             args.hadithBook?.let {
                 var hadith = ""
                 var translatedHadith = ""
+                val grades = StringBuilder()
                 it.hadith?.forEach { info ->
                     if ("ar" == info.lang) {
                         hadith = info.body ?: ""
+                        try {
+                            grades.append(info.grades?.get(0)?.graded_by)
+                        } catch (e: Exception) {
+                        }
+
                     } else if ("en" == info.lang) {
                         translatedHadith = info.body ?: ""
+                        try {
+                            val enGrades = info.grades?.get(0)?.graded_by
+                            if (!enGrades.isNullOrBlank() && grades.isNotBlank()) {
+                                grades.append(" - ")
+                                grades.append(enGrades)
+                            }
+                        } catch (e: Exception) {
+                        }
+
                     }
                 }
                 fragmentHadithDetailsBinding.hadithTextView.text =
                     HtmlCompat.fromHtml(hadith, HtmlCompat.FROM_HTML_MODE_COMPACT)
                 fragmentHadithDetailsBinding.translatedHadithTextView.text =
                     HtmlCompat.fromHtml(translatedHadith, HtmlCompat.FROM_HTML_MODE_COMPACT)
-                fragmentHadithDetailsBinding.titleTranslateTextView.text = getString(R.string.translate)
+                fragmentHadithDetailsBinding.titleTranslateTextView.text =
+                    getString(R.string.translate)
+                fragmentHadithDetailsBinding.gradeByTextView.text = grades
+
             }
         }
     }
